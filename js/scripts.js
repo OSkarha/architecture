543698db31c45d49bdf7c1f6e126e16bef6706df
OSkarha <48570881+OSkarha@users.noreply.github.com>
2020-02-04 16:42:13 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+(function(){ 
+   $('.slider').slick({
+
+     
+    dotsClass: "slider__dots slider__dots--vert"
+
+   });
+
+   // $('.news__slider').slick({
+   //    slidesToShow: 3,
+   //    slidesToScroll: 1,
+   //    autoplay: true,
+   //    autoplaySpeed: 4000,
+   //    dots: true,
+   //    dotsClass: "slider__dots slider__dots--gor",
+   //    arrows: true,
+   //    prevArrow: `<a href="#" class="arrow arrow--left"></a>`,
+   //    nextArrow: `<a href="#" class="arrow arrow--right"></a>`,
+   //    pauseOnFocus: true,
+   //  });
+
+   })();
